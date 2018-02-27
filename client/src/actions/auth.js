2180ad127af5bfd657ca2e8fc03742c17f11b91a
Zoe Larsen <zlarsen.zl@gmail.com>
2018-02-27 15:32:08 +0100

    first commit

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,11 +9,11 @@ export const registerUser = (email, password, passwordConfirmation, history) => 
         dispatch({ type: 'LOGIN', user, headers });
         history.push('/');
       })
-      .catch( err => {
-        const message = res.response.data.errors.full_messages.join(',');
-        dispatch({ type: 'SET_HEADERS', headers: err.headers });
-        dispatch(setFlash(message, 'red'));
-      });
+      // .catch( err => {
+      //   const message = res.response.data.errors.full_messages.join(',');
+      //   dispatch({ type: 'SET_HEADERS', headers: err.headers });
+      //   dispatch(setFlash(message, 'red'));
+      // });
   }
 }
 
@@ -25,11 +25,11 @@ export const handleLogout = (history) => {
         dispatch(setFlash('Logged out successfully!', 'success'));
         history.push('/login');
       })
-      .catch( err => {
-        const message = res.response.data.errors.full_messages.join(',');
-        dispatch({ type: 'SET_HEADERS', headers: err.headers });
-        dispatch(setFlash(message, 'red'));
-      });
+      // .catch( err => {
+      //   const message = res.response.data.errors.full_messages.join(',');
+      //   dispatch({ type: 'SET_HEADERS', headers: err.headers });
+      //   dispatch(setFlash(message, 'red'));
+      // });
   }
 }
 
@@ -41,11 +41,11 @@ export const handleLogin = (email, password, history) => {
         dispatch({ type: 'LOGIN', user, headers });
         history.push('/');
       })
-      .catch( err => {
-        const message = res.response.data.errors.full_messages.join(',');
-        dispatch({ type: 'SET_HEADERS', headers: err.headers })
-        dispatch(setFlash(message, 'red'));
-      })
+      // .catch( err => {
+      //   const message = res.response.data.errors.full_messages.join(',');
+      //   dispatch({ type: 'SET_HEADERS', headers: err.headers })
+      //   dispatch(setFlash(message, 'red'));
+      // })
   }
 }
 
@@ -67,10 +67,10 @@ export const acceptInvitation = (invite, history) => {
         dispatch(setFlash('Welcome to Dash! Please log in.', 'blue'))
         history.push('/login');
       })
-      .catch( err => {
-        const message = res.response.data.errors;
-        dispatch({ type: 'SET_HEADERS', headers: err.headers });
-        dispatch(setFlash(message, 'red'));
-      });
+      // .catch( err => {
+      //   const message = res.response.data.errors;
+      //   dispatch({ type: 'SET_HEADERS', headers: err.headers });
+      //   dispatch(setFlash(message, 'red'));
+      // });
   }
 }
